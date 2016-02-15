8fa52a647cc8e532087b563dcf1a45253470d535
mnicoletti15 <mnicoletti@berkeley.edu>
2016-02-14 21:58:42 -0800

    updated with function that computes the row reduced echelon form of a matrix

--- matreducalg.py
+++ matreducalg.py
@@ -1,5 +1,8 @@
 import copy
 ##Matrices are represented by lists of lists: eg. [[1, 3], [1, 5]]
+# 							is the matrix | 1  3 |
+##										  | 1  5 |	
+#
 
 
 def get_index(A, i, j):
@@ -90,14 +93,51 @@ def echelon_form(A):
 		else:
 			return recursive_call1(delete_down(A))
 
+
+def is_leading_entry(A, i, j):
+	row = get_row(A, i)
+	col = get_col(A, j)
+	if get_index(A, i, j)==0:
+		return False
+	for item in row[:j-1]:
+		if item != 0:
+			return False
+	for item in col[i:]:
+		if item != 0:
+			return False
+	return True
+
+
+
+def rref(A):
+	C = echelon_form(A)
+	B = copy.deepcopy(C)
+	for i in range(1, len(B)+1)[::-1]:
+		for j in range(1, len(get_row(B, 1))+1)[::-1]:
+			if is_leading_entry(C, i, j):
+				B[i-1] = mul_vector(1/get_index(B, i, j), B[i-1])
+				for q in range(1, i):
+					mult = -get_index(B, q, j)
+					v = mul_vector(mult, get_row(B, i))
+					B[q-1] = add_vectors(v, B[q-1])
+	return B
+
+
+
 def print_matrix(A):
 	for row in A:
 		print(row)
 
 
-A = [[1, 4, 5, -9, -7], [-1, -2, -1, 3, 1], [-2, -3, 0, 3, -1], [0, -3, -6, 4, 9], [0, 0, 0, 0, 0]]
-B = Xij(A)
-print_matrix(echelon_form(A))
+A = [[-1, 0, -1, 4, 12, 30],\
+     [4, -1, 0, -1, 10, 30],\
+     [0, -1, 4, -1, 9, 70],\
+     [-1, 4, -1, 0, 9, 60],\
+     [1, -2, -3, 4, 5, 23]]
+B = rref(A)
+print_matrix(B)
+
+
 
 
 
